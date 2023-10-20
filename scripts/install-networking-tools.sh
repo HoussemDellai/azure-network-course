fb8b3647b24d78b3f85b37a5b2c81a0ab256a5a9
Houssem Dellai <hodellai@microsoft.com>
2023-10-20 09:46:55 +0200

    added _01_hub_spoke_nva

--- scripts/install-networking-tools.sh
+++ scripts/install-networking-tools.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# update packages
-sudo apt update
+# Update the system
+sudo apt-get update -y
+
+# Upgrade packages
+sudo apt-get upgrade -y
 
 # enable IP forwarding
 bash -c "echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf"
 sysctl -p /etc/sysctl.conf
 
 # install bind9
-sudo apt install bind9
+sudo apt-get install bind9 -y
