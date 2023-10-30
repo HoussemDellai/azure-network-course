a4ec4f59fb9e20926586dafb6e4458cff55739dd
Houssem Dellai <hodellai@microsoft.com>
2023-10-30 20:22:05 +0100

    added _02_bastion

--- scripts/install-webapp.sh
+++ scripts/install-webapp.sh
@@ -2,15 +2,15 @@
 
 sudo apt -qq update
 
-# Install curl and traceroute and jq
-sudo apt -qq install curl traceroute inetutils-traceroute jq -y
+# # Install curl and traceroute and jq
+# sudo apt -qq install curl traceroute inetutils-traceroute jq -y
 
-# Install NGINX web server
-sudo apt -qq install nginx -y
+# # Install NGINX web server
+# sudo apt -qq install nginx -y
 
-IP=$(hostname -i)
+# IP=$(hostname -i)
 
-echo "Hello from virtual machine: $HOSTNAME, with IP address: $IP" > /var/www/html/index.html
+# echo "Hello from virtual machine: $HOSTNAME, with IP address: $IP" > /var/www/html/index.html
 
 ################################################################
 # Optional, change command line style
