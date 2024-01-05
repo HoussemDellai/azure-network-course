72cd55e33f2c15908dd8246c70e8293ea4e2c801
Houssem Dellai <hodellai@microsoft.com>
2024-01-05 13:08:45 +0100

    edited output

--- 510_application_gateway_vmss_tls_cert/output.tf
+++
@@ -1,7 +0,0 @@
-output "appgateway_ip" {
-  value = azurerm_public_ip.pip-appgateway.ip_address
-}
-
-output "custom_domain_name" {
-  value = var.custom_domain_name
-}
--- 510_application_gateway_vmss_tls_cert/rg.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-appgateway-vmss-tls-${var.prefix}"
-  location = "westeurope"
-}
