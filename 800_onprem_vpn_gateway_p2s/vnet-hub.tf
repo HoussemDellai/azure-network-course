f6589839fbfc85750314ac3276d8f22f9037088e
Houssem Dellai <houssem.dellai@live.com>
2024-01-29 16:07:42 +0100

    added 810 net gateway

--- 800_onprem_vpn_gateway_p2s/vnet-hub.tf
+++ 800_onprem_vpn_gateway_p2s/vnet-hub.tf
@@ -6,16 +6,16 @@ resource "azurerm_virtual_network" "vnet-hub" {
   dns_servers         = null
 }
 
-resource "azurerm_subnet" "subnet-gateway" {
-  name                 = "GatewaySubnet"
+resource "azurerm_subnet" "subnet-vm" {
+  name                 = "subnet-vm"
   resource_group_name  = azurerm_virtual_network.vnet-hub.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet-hub.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
-resource "azurerm_subnet" "subnet-vm" {
-  name                 = "subnet-vm"
+resource "azurerm_subnet" "subnet-gateway" {
+  name                 = "GatewaySubnet"
   resource_group_name  = azurerm_virtual_network.vnet-hub.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet-hub.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
