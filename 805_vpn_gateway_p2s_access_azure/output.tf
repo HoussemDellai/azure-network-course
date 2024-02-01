4dca5c73c9a196c81690f01989c09aac3840b6bd
Houssem Dellai <houssem.dellai@live.com>
2024-02-01 13:44:38 +0100

    added 810 net gateway

--- 805_vpn_gateway_p2s_access_azure/output.tf
+++ 805_vpn_gateway_p2s_access_azure/output.tf
@@ -5,11 +5,3 @@ output "vm_linux_hub_private_ip" {
 output "vm_windows_hub_private_ip" {
   value = azurerm_windows_virtual_machine.vm.private_ip_address
 }
-
-output "storage_account_url" {
-  value = azurerm_storage_account.sa.primary_blob_endpoint
-}
-
-output "storage_account_blob_url" {
-  value = azurerm_storage_blob.blob.url
-}
