4f8ec1b8dffeeea77b3fc513e60049314338d693
Houssem Dellai <houssem.dellai@live.com>
2024-02-02 19:50:23 +0100

    added 815 vpn gateway

--- 800_vpn_gateway_p2s_onprem/vpn_gateway.tf
+++ 800_vpn_gateway_p2s_onprem/vpn_gateway.tf
@@ -2,7 +2,8 @@ resource "azurerm_public_ip" "pip-vnet-gateway" {
   name                = "pip-vnet-gateway"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_virtual_network_gateway" "vpn-gateway" {
---
+++ 815_vpn_gateway_p2s_access_internet/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_version = ">= 1.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.89.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
