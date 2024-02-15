10a5d1bc7e8624e43944b8f69a1ef8574f71daa4
Houssem Dellai <houssem.dellai@live.com>
2024-02-15 16:38:15 +0100

    working on 851

--- 851_dns_resolver_onprem_to_azure/output.tf
+++ 851_dns_resolver_onprem_to_azure/output.tf
@@ -1,11 +1,3 @@
-output "vm_linux_hub_private_ip" {
-  value = azurerm_linux_virtual_machine.vm-hub.private_ip_address
-}
-
-output "vm_linux_spoke_private_ip" {
-  value = azurerm_linux_virtual_machine.vm-spoke.private_ip_address
-}
-
 output "dns_private_resolver_inbound_ip" {
   value = azurerm_private_dns_resolver_inbound_endpoint.inbound_endpoint.ip_configurations.0.private_ip_address
 }
