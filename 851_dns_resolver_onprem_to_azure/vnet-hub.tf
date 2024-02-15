1fc6c65c7d1e959896ad6c623ed526d6b7b0e32c
Houssem Dellai <houssem.dellai@live.com>
2024-02-15 16:38:15 +0100

    working on 851

--- 851_dns_resolver_onprem_to_azure/vnet-hub.tf
+++ 851_dns_resolver_onprem_to_azure/vnet-hub.tf
@@ -6,33 +6,11 @@ resource "azurerm_virtual_network" "vnet-hub" {
   dns_servers         = null
 }
 
-resource "azurerm_subnet" "subnet-vm" {
-  name                 = "subnet-vm"
-  resource_group_name  = azurerm_virtual_network.vnet-hub.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet-hub.name
-  address_prefixes     = ["10.0.0.0/24"]
-}
-
 resource "azurerm_subnet" "subnet-inbound-dns" {
   name                 = "subnet-inbound-dns"
   resource_group_name  = azurerm_virtual_network.vnet-hub.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet-hub.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "Microsoft.Network.dnsResolvers"
-    service_delegation {
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
-      name    = "Microsoft.Network/dnsResolvers"
-    }
-  }
-}
-
-resource "azurerm_subnet" "subnet-outbound-dns" {
-  name                 = "subnet-outbound-dns"
-  resource_group_name  = azurerm_virtual_network.vnet-hub.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet-hub.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.0.0/24"]
 
   delegation {
     name = "Microsoft.Network.dnsResolvers"
@@ -47,5 +25,5 @@ resource "azurerm_subnet" "subnet-gateway" {
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_virtual_network.vnet-hub.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet-hub.name
-  address_prefixes     = ["10.0.3.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
