4e059fa45b52d5cc40f9a6737629dadf09b614b4
Houssem Dellai <houssem.dellai@live.com>
2024-02-16 18:30:56 +0100

    working on xxx

---
+++ xxx_onprem_simulated/vm_linux-onprem.tf
@@ -0,0 +1,42 @@
+resource "azurerm_network_interface" "nic-vm-onprem" {
+  name                 = "nic-vm-onprem"
+  resource_group_name  = azurerm_resource_group.rg-onprem.name
+  location             = azurerm_resource_group.rg-onprem.location
+  enable_ip_forwarding = false
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet-onprem-vm.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = null
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm-onprem" {
+  name                            = "vm-linux-onprem"
+  resource_group_name             = azurerm_resource_group.rg-onprem.name
+  location                        = azurerm_resource_group.rg-onprem.location
+  size                            = "Standard_B2ats_v2"
+  disable_password_authentication = false
+  admin_username                  = "azureuser"
+  admin_password                  = "@Aa123456789"
+  network_interface_ids           = [azurerm_network_interface.nic-vm-onprem.id]
+  priority                        = "Spot"
+  eviction_policy                 = "Deallocate"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  boot_diagnostics {
+    storage_account_uri = null
+  }
+}
---
+++ xxx_onprem_simulated/vnet-hub.tf
@@ -0,0 +1,29 @@
+resource "azurerm_virtual_network" "vnet-hub" {
+  name                = "vnet-hub"
+  resource_group_name = azurerm_resource_group.rg-hub.name
+  location            = azurerm_resource_group.rg-hub.location
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = null
+}
+
+resource "azurerm_subnet" "subnet-inbound-dns" {
+  name                 = "subnet-inbound-dns"
+  resource_group_name  = azurerm_virtual_network.vnet-hub.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet-hub.name
+  address_prefixes     = ["10.0.0.0/24"]
+
+  delegation {
+    name = "Microsoft.Network.dnsResolvers"
+    service_delegation {
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+      name    = "Microsoft.Network/dnsResolvers"
+    }
+  }
+}
+
+resource "azurerm_subnet" "subnet-gateway" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_virtual_network.vnet-hub.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet-hub.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
