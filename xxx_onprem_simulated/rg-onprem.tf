32156613ae6e11ea43974bfef3a0edd7c05b4aa6
Houssem Dellai <houssem.dellai@live.com>
2024-02-16 18:30:56 +0100

    working on xxx

---
+++ xxx_onprem_simulated/rg-onprem.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg-onprem" {
+  name     = "rg-onprem-${var.prefix}"
+  location = "westeurope"
+}
---
+++ xxx_onprem_simulated/vnet-onprem.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network" "vnet-onprem" {
+  name                = "onpremise-vnet"
+  location            = azurerm_resource_group.rg-onprem.location
+  resource_group_name = azurerm_resource_group.rg-onprem.name
+  address_space       = ["172.16.0.0/12"]
+  dns_servers         = null
+}
+
+resource "azurerm_subnet" "subnet-onprem-vpngateway" {
+    name                    = "GatewaySubnet"
+    resource_group_name     = azurerm_virtual_network.vnet-onprem.resource_group_name
+    virtual_network_name    = azurerm_virtual_network.vnet-onprem.name
+    address_prefixes        = ["172.16.0.0/24"]
+}
+
+resource "azurerm_subnet" "subnet-onprem-vm" {
+    name                    = "subnet-default"
+    resource_group_name     = azurerm_virtual_network.vnet-onprem.resource_group_name
+    virtual_network_name    = azurerm_virtual_network.vnet-onprem.name
+    address_prefixes        = ["172.16.1.0/24"]
+}
