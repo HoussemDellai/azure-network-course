a4b10d26532f7386814d0a99a9dfc5c557951666
Houssem Dellai <houssem.dellai@live.com>
2024-02-21 18:36:09 +0100

    working on 820

---
+++ 820_vpn_gateway_s2s/README.md
@@ -0,0 +1,30 @@
+# Simulating an on premise environment on Azure using VPN Gateway S2S
+
+The objective of this lab is to use `Site to Site VPN Gateway` to connect on-prem environment to Azure virtual network `VNET Hub`.
+The following architecture shows the different components.
+
+![](images/architecture.png)
+
+Note that the hub VM and the on-prem VM will be used to verify connectivity between on-prem and hub network.
+
+First, you need to deploy the resources through `Terraform` by running:
+
+```sh
+terraform init
+terraform plan -out tfplan
+terraform apply tfplan
+```
+
+It will take about 40 minutes to deploy all resources.
+
+Then you can connect to both VMs and verify they connect to each other.
+You need to get the private IP address for each VM.
+Each VM run a sample Nginx app. You will run `curl` against the web app running on these VMs.
+
+```sh
+curl 172.16.1.4
+# Hello from virtual machine: vm-linux-onprem, with IP address: 172.16.1.4
+
+curl 10.0.2.4
+# Hello from virtual machine: vm-linux-hub, with IP address: 10.0.2.4
+```
--- 820_vpn_gateway_s2s/vnet-onprem.tf
+++ 820_vpn_gateway_s2s/vnet-onprem.tf
@@ -19,33 +19,3 @@ resource "azurerm_subnet" "subnet-onprem-vm" {
   virtual_network_name = azurerm_virtual_network.vnet-onprem.name
   address_prefixes     = ["172.16.1.0/24"]
 }
-
-resource "azurerm_subnet" "subnet-onprem-dns-inbound" {
-  name                 = "subnet-onprem-dns-inbound"
-  resource_group_name  = azurerm_virtual_network.vnet-onprem.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet-onprem.name
-  address_prefixes     = ["172.16.2.0/24"]
-
-  delegation {
-    name = "Microsoft.Network.dnsResolvers"
-    service_delegation {
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
-      name    = "Microsoft.Network/dnsResolvers"
-    }
-  }
-}
-
-resource "azurerm_subnet" "subnet-onprem-dns-outbound" {
-  name                 = "subnet-onprem-dns-outbound"
-  resource_group_name  = azurerm_virtual_network.vnet-onprem.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet-onprem.name
-  address_prefixes     = ["172.16.3.0/24"]
-
-  delegation {
-    name = "Microsoft.Network.dnsResolvers"
-    service_delegation {
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
-      name    = "Microsoft.Network/dnsResolvers"
-    }
-  }
-}
