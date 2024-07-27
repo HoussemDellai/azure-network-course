7d0c233ae4f23f41f230791df6a8dc5e7551d863
Houssem Dellai <hodellai@microsoft.com>
2024-07-27 18:52:29 +0200

    edited sfd

--- 551_frontdoor_appservice_multi_region/output.tf
+++ 551_frontdoor_appservice_multi_region/output.tf
@@ -1,3 +1,15 @@
-output "frontdoor-endpoint-app" {
+output "frontdoor_endpoint_url" {
   value = azurerm_cdn_frontdoor_endpoint.endpoint.host_name
+}
+
+output "appservice_url_region1" {
+  value = azurerm_linux_web_app.region1.default_hostname
+}
+
+output "appservice_url_region2" {
+  value = azurerm_linux_web_app.region2.default_hostname
+}
+
+output "appservice_url_region3" {
+  value = azurerm_linux_web_app.region3.default_hostname
 }
