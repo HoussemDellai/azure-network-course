888bcdca32590052d10e997f38a6150527a0cd19
Houssem Dellai <hodellai@microsoft.com>
2024-07-27 16:59:45 +0200

    edited sfd

---
+++ 551_frontdoor_appservice_multi_region/frontdoor-origin-region3.tf
@@ -0,0 +1,12 @@
+resource "azurerm_cdn_frontdoor_origin" "origin-region3" {
+  name                           = "region3"
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.origin-group.id
+  enabled                        = true
+  host_name                      = azurerm_linux_web_app.region3.default_hostname
+  http_port                      = 80
+  https_port                     = 443
+  origin_host_header             = azurerm_linux_web_app.region3.default_hostname
+  priority                       = 1
+  weight                         = 1000
+  certificate_name_check_enabled = true
+}
