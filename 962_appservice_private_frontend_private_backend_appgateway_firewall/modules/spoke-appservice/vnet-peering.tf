193d20d7d8e1bf9107c9aed153da0c72f75a7562
Houssem Dellai <hodellai@microsoft.com>
2025-02-20 16:19:32 +0100

    in progress

--- 962_appservice_private_frontend_private_backend_appgateway_firewall/modules/spoke-appservice/vnet-peering.tf
+++ 962_appservice_private_frontend_private_backend_appgateway_firewall/modules/spoke-appservice/vnet-peering.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network_peering" "vnet-peering-hub-to-spoke" {
-  name                         = "vnet-peering-hub-to-spoke"
+  name                         = "vnet-peering-hub-to-spoke-${var.prefix}"
   virtual_network_name         = var.hub_vnet_name
   resource_group_name          = var.hub_vnet_rg_name
   remote_virtual_network_id    = azurerm_virtual_network.vnet-spoke.id
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network_peering" "vnet-peering-hub-to-spoke" {
 }
 
 resource "azurerm_virtual_network_peering" "vnet-peering-spoke-to-hub" {
-  name                         = "vnet-peering-spoke-to-hub"
+  name                         = "vnet-peering-spoke-to-hub-${var.prefix}"
   virtual_network_name         = azurerm_virtual_network.vnet-spoke.name
   resource_group_name          = azurerm_virtual_network.vnet-spoke.resource_group_name
   remote_virtual_network_id    = var.hub_vnet_id
