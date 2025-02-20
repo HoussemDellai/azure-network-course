7b4c91769afde38164595bbb928e7bde54983cad
Houssem Dellai <hodellai@microsoft.com>
2025-02-20 15:41:03 +0100

    in progress

--- 962_appservice_private_frontend_private_backend_appgateway_firewall/modules/spoke-appservice/private-dns-zone.tf
+++ 962_appservice_private_frontend_private_backend_appgateway_firewall/modules/spoke-appservice/private-dns-zone.tf
@@ -7,5 +7,5 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dns-zone-link" {
   name                  = "dns-zone-link"
   resource_group_name   = azurerm_resource_group.rg.name
   private_dns_zone_name = azurerm_private_dns_zone.dns-appservice.name
-  virtual_network_id    = azurerm_virtual_network.vnet-app.id
+  virtual_network_id    = azurerm_virtual_network.vnet-spoke.id
 }
