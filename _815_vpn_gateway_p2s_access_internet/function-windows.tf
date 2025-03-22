fd2afc8a8ab8df48d6c4554779b71dafdc67083e
Houssem Dellai <hodellai@microsoft.com>
2025-03-22 07:53:01 +0100

    work in progress

---
+++ _815_vpn_gateway_p2s_access_internet/function-windows.tf
@@ -0,0 +1,30 @@
+resource "azurerm_storage_account" "storage" {
+  name                     = "storagefunction7531"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "app-service-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "function" {
+  name                       = "function-app"
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  service_plan_id            = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      powershell_core_version = 7
+    }
+  }
+}
