a204c24e09abab66fcfd6a89e426f36dbea1f495
Houssem Dellai <hodellai@microsoft.com>
2025-06-01 07:00:02 +0200

    work in progress 818

--- 817_vpn_gateway_p2s_access_internet_fw_basic/log_analytics.tf
+++ 817_vpn_gateway_p2s_access_internet_fw_basic/log_analytics.tf
@@ -62,19 +62,11 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostics-vpngateway" {
     }
   }
 
-<<<<<<< HEAD
   dynamic "enabled_metric" {
     for_each = data.azurerm_monitor_diagnostic_categories.categories-vpngateway.metrics
 
     content {
       category = enabled_metric.key
-=======
-  dynamic "metric" {
-    for_each = data.azurerm_monitor_diagnostic_categories.categories-vpngateway.metrics
-
-    content {
-      category = metric.key
->>>>>>> a26231ceb327f6bd30df487e9393348a0f3f689f
     }
   }
 
