cb5beccef6ef503c7306785f5f97eb597bd659ee
Houssem Dellai <hodellai@microsoft.com>
2025-06-01 14:25:59 +0200

    work in progress 818

--- 818_vpn_wireguard_mitmproxy_vm/install-mitmproxy-wireguard.ps1
+++ 818_vpn_wireguard_mitmproxy_vm/install-mitmproxy-wireguard.ps1
@@ -5,4 +5,10 @@ winget install -e --id Python.Python.3.13
 pip install mitmproxy
 
 # start mitmproxy and wireguard
-mitmweb --listen-port 8080 --web-host 0.0.0.0 --web-port 8081 --set block_global=false --mode wireguard -w +flows.mitm
+mitmweb --listen-port 8080 --web-host 0.0.0.0 --web-port 8081 --set block_global=false --mode wireguard -s io-write-flow-file.py
+# mitmweb --listen-port 8080 --web-host 0.0.0.0 --web-port 8081 --set block_global=false --mode wireguard -w +flows.mitm
+
+# to connect to wireguard you need:
+# 1. download wireguard client
+# 2. add profile
+# 3. install MITMProxy certificate
