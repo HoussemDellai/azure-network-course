c66c04b215b601e3996ca4ec6a86fae63826e794
Houssem Dellai <hodellai@microsoft.com>
2025-06-01 14:06:18 +0200

    work in progress 818

--- 818_vpn_wireguard_mitmproxy_vm/install-mitmproxy-wireguard.ps1
+++ 818_vpn_wireguard_mitmproxy_vm/install-mitmproxy-wireguard.ps1
@@ -5,4 +5,4 @@ winget install -e --id Python.Python.3.13
 pip install mitmproxy
 
 # start mitmproxy and wireguard
-mitmweb --listen-port 8080 --web-host 127.0.0.1 --web-port 8081 --set block_global=false --mode wireguard -w +flows.mitm
+mitmweb --listen-port 8080 --web-host 0.0.0.0 --web-port 8081 --set block_global=false --mode wireguard -w +flows.mitm
--- 818_vpn_wireguard_mitmproxy_vm/nsg.tf
+++ 818_vpn_wireguard_mitmproxy_vm/nsg.tf
@@ -4,12 +4,26 @@ resource "azurerm_network_security_group" "nsg-vm" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_network_security_rule" "allow-any" {
+resource "azurerm_network_security_rule" "allow-wireguard-udp" {
   network_security_group_name  = azurerm_network_security_group.nsg-vm.name
   resource_group_name          = azurerm_network_security_group.nsg-vm.resource_group_name
-  name                         = "allow-any"
+  name                         = "allow-wireguard-udp"
   access                       = "Allow"
-  priority                     = 1000
+  priority                     = 100
+  direction                    = "Inbound"
+  protocol                     = "Udp"
+  source_address_prefix        = "*"
+  source_port_range            = "*"
+  destination_address_prefixes = ["0.0.0.0/0"]
+  destination_port_range       = "*"
+}
+
+resource "azurerm_network_security_rule" "allow-all-tcp" {
+  network_security_group_name  = azurerm_network_security_group.nsg-vm.name
+  resource_group_name          = azurerm_network_security_group.nsg-vm.resource_group_name
+  name                         = "allow-all-tcp"
+  access                       = "Allow"
+  priority                     = 200
   direction                    = "Inbound"
   protocol                     = "Tcp"
   source_address_prefix        = "*"
