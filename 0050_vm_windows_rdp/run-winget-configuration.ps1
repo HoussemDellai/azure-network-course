89e02f1fd1529ae3c1060e60307711880efe7d66
Houssem Dellai <hodellai@microsoft.com>
2025-06-02 23:24:30 +0200

    work in progress winget configuration 0050

--- 0050_vm_windows_rdp/run-winget-configuration.ps1
+++ 0050_vm_windows_rdp/run-winget-configuration.ps1
@@ -1,3 +1,4 @@
 winget configure --enable
 
-winget configure -f ./.config/configuration.winget
+winget configure -f https://raw.githubusercontent.com/HoussemDellai/azure-network-course/refs/heads/main/0050_vm_windows_rdp/.config/configuration.winget
+# winget configure -f ./.config/configuration.winget
