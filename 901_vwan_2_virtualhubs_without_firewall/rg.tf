4f3117162894fc37b7f2b8c8d6ac54dbdea42bf6
Houssem Dellai <hodellai@microsoft.com>
2025-07-17 21:29:42 +0200

    work in progress

--- 901_vwan_2_virtualhubs_without_firewall/rg.tf
+++ 901_vwan_2_virtualhubs_without_firewall/rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-virtualwan-virtualhub-${var.prefix}"
+  name     = "rg-virtualwan-2-virtualhubs-${var.prefix}"
   location = var.region1
 }
