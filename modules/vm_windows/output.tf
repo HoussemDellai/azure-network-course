f981a9e136abd4ffcccd7db7a5fb37d4e3f6c738
Houssem Dellai <hodellai@microsoft.com>
2025-07-23 18:52:09 +0200

    work in progress

--- modules/vm_windows/output.tf
+++ modules/vm_windows/output.tf
@@ -6,6 +6,6 @@ output "vm_private_ip" {
   value = azurerm_windows_virtual_machine.vm.private_ip_address
 }
 
-output "public_ip" {
+output "vm_public_ip" {
   value = var.enable_public_ip ? azurerm_public_ip.pip_vm.0.ip_address : null
 }
