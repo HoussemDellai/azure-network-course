dc52c19203c57930ddc25797c38b3adb95154b4a
Houssem Dellai <hodellai@microsoft.com>
2025-07-25 15:33:55 +0200

    work in progress

--- 0050_vm_windows_rdp/nsg.tf
+++ 0050_vm_windows_rdp/nsg.tf
@@ -4,10 +4,10 @@ resource "azurerm_network_security_group" "nsg-vm" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_network_security_rule" "allow-rdp" {
+resource "azurerm_network_security_rule" "allow-me-rdp" {
   network_security_group_name  = azurerm_network_security_group.nsg-vm.name
   resource_group_name          = azurerm_network_security_group.nsg-vm.resource_group_name
-  name                         = "allow-rdp"
+  name                         = "allow-me-rdp"
   access                       = "Allow"
   priority                     = 1000
   direction                    = "Inbound"
@@ -16,6 +16,34 @@ resource "azurerm_network_security_rule" "allow-rdp" {
   source_port_range            = "*"
   destination_address_prefixes = ["0.0.0.0/0"]
   destination_port_range       = "3389"
+}
+
+resource "azurerm_network_security_rule" "allow-all-tcp" {
+  network_security_group_name  = azurerm_network_security_group.nsg-vm.name
+  resource_group_name          = azurerm_network_security_group.nsg-vm.resource_group_name
+  name                         = "allow-all-tcp"
+  access                       = "Allow"
+  priority                     = 100
+  direction                    = "Inbound"
+  protocol                     = "Tcp"
+  source_address_prefix        = "*"
+  source_port_range            = "*"
+  destination_address_prefixes = ["0.0.0.0/0"]
+  destination_port_range       = "*"
+}
+
+resource "azurerm_network_security_rule" "allow-all-udp" {
+  network_security_group_name  = azurerm_network_security_group.nsg-vm.name
+  resource_group_name          = azurerm_network_security_group.nsg-vm.resource_group_name
+  name                         = "allow-all-udp"
+  access                       = "Allow"
+  priority                     = 101
+  direction                    = "Inbound"
+  protocol                     = "Udp"
+  source_address_prefix        = "*"
+  source_port_range            = "*"
+  destination_address_prefixes = ["0.0.0.0/0"]
+  destination_port_range       = "*"
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg-association" {
