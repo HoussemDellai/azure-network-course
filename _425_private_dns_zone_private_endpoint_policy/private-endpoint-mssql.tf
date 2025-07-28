4ab23e87cf012cf987b7551d0ecac034d8e76866
Houssem Dellai <hodellai@microsoft.com>
2025-07-29 00:02:23 +0200

    work in progress

--- _425_private_dns_zone_private_endpoint_policy/private-endpoint-mssql.tf
+++ _425_private_dns_zone_private_endpoint_policy/private-endpoint-mssql.tf
@@ -1,11 +1,12 @@
 resource "azurerm_private_endpoint" "private-endpoint-mssql" {
-  name                = "private-endpoint-mssql"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.snet-pe.id
+  name                          = "pe-mssql"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  subnet_id                     = azurerm_subnet.snet-pe.id
+  custom_network_interface_name = "nic-pe-mssql"
 
   private_service_connection {
-    name                           = "private-service-connection"
+    name                           = "connection"
     private_connection_resource_id = azurerm_mssql_server.mssql-server.id
     subresource_names              = ["sqlServer"]
     is_manual_connection           = false
