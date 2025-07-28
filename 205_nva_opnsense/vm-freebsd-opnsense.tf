5b502e47ff1e9b0c2a764a83933f6871133ac4d1
Houssem Dellai <hodellai@microsoft.com>
2025-07-28 21:41:37 +0200

    work in progress

--- 205_nva_opnsense/vm-freebsd-opnsense.tf
+++ 205_nva_opnsense/vm-freebsd-opnsense.tf
@@ -37,14 +37,14 @@ resource "azurerm_linux_virtual_machine" "vm-nva" {
   name                            = "vm-nva-opnsense"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  size                            = "Standard_D4ads_v6" # "Standard_D96ads_v5" # 
+  size                            = "Standard_B2s" # "Standard_D96ads_v5" # 
   disable_password_authentication = false
   admin_username                  = "azureuser"
   admin_password                  = "@Aa123456789"
-  priority                        = "Spot"
-  eviction_policy                 = "Delete" # "Deallocate" # With Spot, there's no option of Stop-Deallocate for Ephemeral VMs, rather users need to Delete instead of deallocating them.
+  # priority                        = "Spot"
+  # eviction_policy                 = "Delete" # "Deallocate" # With Spot, there's no option of Stop-Deallocate for Ephemeral VMs, rather users need to Delete instead of deallocating them.
   network_interface_ids           = [azurerm_network_interface.nic-vm-nva-untrusted.id, azurerm_network_interface.nic-vm-nva-trusted.id]
-  disk_controller_type            = "NVMe" # "SCSI" # "IDE" # "SCSI" is the default value. "NVMe" is only supported for Ephemeral OS Disk.
+  # disk_controller_type            = "NVMe" # "SCSI" # "IDE" # "SCSI" is the default value. "NVMe" is only supported for Ephemeral OS Disk.
 
   # custom_data = filebase64("../scripts/install-tools-windows.ps1")
   # custom_data = filebase64("./run-winget-configuration.ps1")
@@ -56,10 +56,10 @@ resource "azurerm_linux_virtual_machine" "vm-nva" {
     storage_account_type = "StandardSSD_LRS" # "Standard_LRS"
     disk_size_gb         = 128
 
-    diff_disk_settings {
-      option    = "Local"    # Specifies the Ephemeral Disk Settings for the OS Disk. At this time the only possible value is Local.
-      placement = "NvmeDisk" # "ResourceDisk" # "CacheDisk" # Specifies the Ephemeral Disk Placement for the OS Disk. NvmeDisk can only be used for v6 VMs
-    }
+    # diff_disk_settings {
+    #   option    = "Local"    # Specifies the Ephemeral Disk Settings for the OS Disk. At this time the only possible value is Local.
+    #   placement = "NvmeDisk" # "ResourceDisk" # "CacheDisk" # Specifies the Ephemeral Disk Placement for the OS Disk. NvmeDisk can only be used for v6 VMs
+    # }
   }
 
   source_image_reference {
@@ -87,6 +87,61 @@ resource "azurerm_linux_virtual_machine" "vm-nva" {
     ignore_changes = [identity]
   }
 }
+
+# resource "azurerm_linux_virtual_machine" "vm-nva-spot" {
+#   name                            = "vm-nva-opnsense"
+#   resource_group_name             = azurerm_resource_group.rg.name
+#   location                        = azurerm_resource_group.rg.location
+#   size                            = "Standard_D4ads_v6" # "Standard_D96ads_v5" # 
+#   disable_password_authentication = false
+#   admin_username                  = "azureuser"
+#   admin_password                  = "@Aa123456789"
+#   priority                        = "Spot"
+#   eviction_policy                 = "Delete" # "Deallocate" # With Spot, there's no option of Stop-Deallocate for Ephemeral VMs, rather users need to Delete instead of deallocating them.
+#   network_interface_ids           = [azurerm_network_interface.nic-vm-nva-untrusted.id, azurerm_network_interface.nic-vm-nva-trusted.id]
+#   disk_controller_type            = "NVMe" # "SCSI" # "IDE" # "SCSI" is the default value. "NVMe" is only supported for Ephemeral OS Disk.
+
+#   # custom_data = filebase64("../scripts/install-tools-windows.ps1")
+#   # custom_data = filebase64("./run-winget-configuration.ps1")
+#   # custom_data = filebase64("../scripts/install-tools-windows-vm-winget-mini.ps1")
+
+#   os_disk {
+#     name                 = "os-disk-vm-nva"
+#     caching              = "ReadOnly"        # "ReadWrite" # None, ReadOnly and ReadWrite.
+#     storage_account_type = "StandardSSD_LRS" # "Standard_LRS"
+#     disk_size_gb         = 128
+
+#     diff_disk_settings {
+#       option    = "Local"    # Specifies the Ephemeral Disk Settings for the OS Disk. At this time the only possible value is Local.
+#       placement = "NvmeDisk" # "ResourceDisk" # "CacheDisk" # Specifies the Ephemeral Disk Placement for the OS Disk. NvmeDisk can only be used for v6 VMs
+#     }
+#   }
+
+#   source_image_reference {
+#     publisher = "thefreebsdfoundation"
+#     offer     = "freebsd-14_1" # "windows11preview-arm64"
+#     sku       = "14_1-release-amd64-gen2-zfs"
+#     version   = "latest"
+#   }
+
+#   plan {
+#     name      = "14_1-release-amd64-gen2-zfs"
+#     publisher = "thefreebsdfoundation"
+#     product   = "freebsd-14_1"
+#   }
+
+#   identity {
+#     type = "SystemAssigned"
+#   }
+
+#   boot_diagnostics {
+#     storage_account_uri = null
+#   }
+
+#   lifecycle {
+#     ignore_changes = [identity]
+#   }
+# }
 
 resource "azurerm_virtual_machine_extension" "cslinux" {
   name                 = "cslinux"
