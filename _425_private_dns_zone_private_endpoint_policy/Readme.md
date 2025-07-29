2553a594446b8806a099c7b2119ed18a431b3a51
Houssem Dellai <hodellai@microsoft.com>
2025-07-29 11:43:19 +0200

    work in progress

--- _425_private_dns_zone_private_endpoint_policy/Readme.md
+++ _425_private_dns_zone_private_endpoint_policy/Readme.md
@@ -1,10 +1,12 @@
 # Automating records creation with Azure Policy
 
 
-Trigger a policy compliance evaluation for a scope.
+> Note that deleting the Policy assignment deletes the remediation as well. This means the DNS records will be removed in our scenario.
+
+> Note that Azure triggers the policies every 24 hours. But you can manually trigger a compliance evaluation using the following command.
 
 ```sh
-az policy state trigger-scan
+az policy state trigger-scan # trigger for all subscription
+# OR
+az policy state trigger-scan -g <resource-group-name> # trigger for a specific resource group
 ```
-
-> Note that deleting the Policy assignment deletes the remediation as well. This means the DNS records will be removed in our scenario.
--- _425_private_dns_zone_private_endpoint_policy/vnet.tf
+++ _425_private_dns_zone_private_endpoint_policy/vnet.tf
@@ -3,7 +3,6 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = null
 }
 
 resource "azurerm_subnet" "snet-pe" {
@@ -11,11 +10,4 @@ resource "azurerm_subnet" "snet-pe" {
   resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
-}
-
-resource "azurerm_subnet" "snet-vm" {
-  name                 = "snet-vm"
-  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
 }
