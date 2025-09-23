5594df339ed0601358401fd119150b36bbf21e56
Houssem Dellai <hodellai@microsoft.com>
2025-09-23 10:16:52 +0200

    work in progress

--- 205_nva_opnsense/hub_rg.tf
+++ 205_nva_opnsense/hub_rg.tf
@@ -1,4 +1,9 @@
 resource "azurerm_resource_group" "rg" {
   name     = "rg-hub-nva-opnsense-${var.prefix}"
   location = "swedencentral" # "swedencentral" #  # "swedencentral"
+
+  tags = {
+    Cost-Control     = "Ignore"
+    Security-Control = "Ignore"
+  }
 }
