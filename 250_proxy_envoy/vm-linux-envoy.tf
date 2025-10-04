a142c27288bc878bc2f90be0599b4d2176676ae2
Houssem Dellai <hodellai@microsoft.com>
2025-10-04 21:05:33 +0200

    work in progress

--- 250_proxy_envoy/vm-linux-envoy.tf
+++ 250_proxy_envoy/vm-linux-envoy.tf
@@ -10,7 +10,7 @@ resource "azurerm_network_interface" "nic-vm-linux" {
   name                  = "nic-vm-linux"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
-  ip_forwarding_enabled = false
+  ip_forwarding_enabled = true
 
   ip_configuration {
     name                          = "internal"
