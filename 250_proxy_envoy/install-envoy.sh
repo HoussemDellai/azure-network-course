38c1634743ea52b85cd1fc639434f7000b09d3ec
Houssem Dellai <hodellai@microsoft.com>
2025-11-22 12:32:02 +0100

    work in progress

--- 250_proxy_envoy/install-envoy.sh
+++ 250_proxy_envoy/install-envoy.sh
@@ -8,7 +8,7 @@ sudo apt-get install envoy
 envoy --version
 
 # run envoy
-sudo wget https://raw.githubusercontent.com/HoussemDellai/azure-network-course/refs/heads/main/250_proxy_envoy/envoy-proxy-transparent-connect.yml
+sudo wget https://raw.githubusercontent.com/HoussemDellai/azure-network-course/refs/heads/main/250_proxy_envoy/envoy-proxy-transparent-connect.yaml
 
-sudo envoy -c envoy-proxy-transparent-connect.yml --log-level debug --log-path /var/log/envoy.log
+sudo envoy -c envoy-proxy-transparent-connect.yaml --log-level debug --log-path /var/log/envoy.log
 
